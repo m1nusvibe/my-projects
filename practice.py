0cf898dbcbb8185bddd3fdb5624d91282fd13be8
m1nusvibe <vooper333@gmail.com>
2026-02-21 17:16:07 +0300

    edit and ready part code

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
 import operator
 
 numbers = {'ноль': 0, 'один': 1, 'два': 2, 'три': 3, 'четыре': 4, 'пять': 5, 'шесть': 6,
-           'семь': 7, 'восемь': 8, 'девать': 9}
+           'семь': 7, 'восемь': 8, 'девять': 9}
 operations = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
 
 operations_symbol = {'минус': '-',
@@ -25,15 +25,24 @@ def tokenize(text):
 
         elif words in operations_symbol:
             ready_text.append(operations_symbol[words])
+
     return ready_text
+def calculate():
+
 
 def calculate(tokens):
     result = tokens[0]
     for i in range(0, len(tokens)):
-        if tokens[i] == '+':
-            result = result + tokens[i+1]
-        elif tokens[i] == '-':
-            result = result - tokens[i+1]
+        if '*' in tokens:
+            result = tokens[tokens.index('*') - 1] * tokens[tokens.index('*') + 1]
+        elif '/' in tokens:
+            result = tokens[tokens.index('/') - 1] / tokens[tokens.index('/') + 1]
+        else:
+            if tokens[i] == '+':
+                result = result + tokens[i+1]
+            elif tokens[i] == '-':
+                result = result - tokens[i+1]
 
+    print(result)
     return result
-calculate(tokenize("два плюс три минус один"))
+calculate(tokenize("два плюс пять разделить пять"))
